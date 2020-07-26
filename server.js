c1554b2a4f970da40bdc11430f765e4fec938c11
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-07-26 08:18:46 -0400

    successfully created new + post

--- server.js
+++ server.js
@@ -32,11 +32,21 @@ app.get('/fruits/', (req, res) => {
 app.get('/fruits/new', (req, res) => {
   res.render('new.ejs')
 })
+
 //create + POST
 app.post('/fruits', (req, res) => {
   // res.send('hi');
-  console.log(req.body);
-  res.send('data receieved');
+  // console.log(req.body);
+  // res.send('data receieved');
+  if(req.body.readyToEat === 'on'){
+      req.body.readyToEat = true;
+  } else {
+      req.body.readyToEat = false;
+  }
+  fruits.push(req.body); //adds the new item to db/array
+  // console.log(fruits); //logs in terminal
+  // res.send('data receieved'); //sends user a message
+  res.redirect('/fruits'); //sends user back to index page
 })
 
 //SHOW route
