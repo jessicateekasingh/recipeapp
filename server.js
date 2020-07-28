b6e585741de2a64571b274c55ed3703d8ae983ee
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-07-27 21:11:14 -0400

    connected with mongodb atlas

--- server.js
+++ server.js
@@ -1,25 +1,60 @@
-const express = require('express');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-
-const app = express();
-const fruitsController = require('./controllers/fruits.js');
-app.use('/fruits', fruitsController);
-
-
-
-//middleware
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
-
-
-
-
-//connections
-mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
-  console.log('The connection with mongod is established')
+// Dependencies
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const app = express()
+const db = mongoose.connection
+require('dotenv').config()
+// PORT
+const PORT = process.env.PORT || 3333
+// Database
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+)
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+// middleware
+// use the public folder
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+// routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(PORT, () => {
+  console.log('listening...🍌🍒🥝🍓🍆', PORT)
 })
 
-app.listen(3000, () => {
-  console.log('listening...🍌🍒🥝🍓🍆');
-})
+
+
+
+
+// const express = require('express');
+// const methodOverride = require('method-override');
+// const mongoose = require('mongoose');
+//
+// const app = express();
+//
+//
+//
+//
+// //middleware
+// app.use(express.urlencoded({extended:true}));
+// app.use(methodOverride('_method'));
+//
+//
+// const fruitsController = require('./controllers/fruits.js');
+// app.use('/fruits', fruitsController);
+//
+//
+// //connections
+// mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
+//   console.log('The connection with mongod is established')
+// })
+//
+// app.listen(3000, () => {
+//   console.log('listening...🍌🍒🥝🍓🍆');
+// })
